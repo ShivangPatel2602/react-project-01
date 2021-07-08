d1d74452f74369f730f7767e45526f0ca1429f53
Shivang Patel <shivang.patel02062002@gmail.com>
2021-07-08 16:21:49 +0530

    Done with the project

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import Card from "../UI/Card";
-import Button from "../UI/Button";
-import ErrorModal from "../UI/ErrorModal";
-import classes from "./AddUser.module.css";
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -11,17 +11,17 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim.length === 0) {
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
-        title: "Invalid input",
-        message: "Please enter a valid name and age (non-empty values).",
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
     if (+enteredAge < 1) {
       setError({
-        title: "Invalid age",
-        message: "Please enter a valid age(> 0).",
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
       });
       return;
     }
